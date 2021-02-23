2a142729a91f5ddec2227037f7d4c6e0106170e5
snehik <snehik1989@gmail.com>
2021-02-23 06:51:35 -0800

    Third Commit

--- src/main/scala/com/snehik/training/test.scala
+++ src/main/scala/com/snehik/training/test.scala
@@ -1,7 +1,22 @@
 package com.snehik.training
 
+
+object higherOrderFunctionDemo{
+  def higherOrderFunction(input:List[Int],f:Int=>Int):Int={
+      val modifiedList=input.map(x=>f(x))
+      val sum = modifiedList.foldLeft(0)((cum,x)=>cum+x)
+      sum
+  }
+}
+
 object test {
   def main(args:Array[String])={
-    println("This is a sample program")
+
+    val input = List(1,2,3,4,5)
+    def square = (x:Int) => x*x
+    def cube = (x:Int) => x*x*x
+
+    println(higherOrderFunctionDemo.higherOrderFunction(input,square))
+    println(higherOrderFunctionDemo.higherOrderFunction(input,cube))
   }
 }
