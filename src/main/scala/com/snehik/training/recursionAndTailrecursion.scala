0103632e247aeecc3652ac4ce1dc38d3fde151a2
snehik <snehik1989@gmail.com>
2021-02-23 21:29:53 -0800

    Commit For Tail Recursion

--- src/main/scala/com/snehik/training/recursionAndTailrecursion.scala
+++ src/main/scala/com/snehik/training/recursionAndTailrecursion.scala
@@ -1,5 +1,26 @@
 package com.snehik.training
 
-object recursionAndTailrecursion {
+object result{
+  def factorial(x:Int):Int={
+    if (x<=0) 1
+    else
+      x*factorial(x-1)
+  }
+
+  def anotherFactorial(x:Int):Int={
+    def helper(x:Int,accumulator:Int):Int={
+      if(x<=1) accumulator
+      else
+        helper(x-1,x*accumulator)
+    }
+    helper(x,1)
+  }
+
+}
+object recursionAndTailRecursion {
+  def main(args:Array[String])= {
+    println(result.factorial(5))
+    println(result.anotherFactorial(5))
+  }
 
 }
